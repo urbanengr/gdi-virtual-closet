a1495acc16cb6ff6a26608ba28fb42f8a5fad2e9
claudiab <claudberumen@gmail.com>
2021-11-11 19:21:07 -0600

    initial restructure of directory

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ImageUploader from './views/image-uploader';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './view/pages/login/login.js';
+import Home from './view/pages/home/home.js';
+ 
+import Signup from './view/pages/signup/signup.js';
+
+ import { Container, Navbar, Nav } from 'react-bootstrap';
+
 function App() {
   return (
-    <div className="App">
-      <header className="">
-        Virtual Closet Nav
-      </header>
-
-      <ImageUploader/>
-    </div>
+    <main>
+     <div className="App">
+       
+        <Navbar bg="light" variant="light">
+          <Container>
+          <Navbar.Brand href="#home">Virtual Closet</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/home">Home</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
+              <Nav.Link href="/signup">Signup</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        </div>   
+      <Router>
+              <div>
+                  <Routes>
+                        <Route path="/" element={<Home/>} exact/>   
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup/>}/>
+                  </Routes>
+                </div>
+            </Router>
+    </main>
   );
 }
 
 export default App;
+
---
+++ src/views/pages/signup/signup.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Signup extends Component{
+    render(){
+        return(
+            <h1>Signup</h1>
+        )
+    }
+}
+export default Signup;
